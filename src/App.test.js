a8504ccc400f60880d868aba3e79fe5905fd30bf
gurungsl <105928096+gurungsl@users.noreply.github.com>
2022-05-21 16:27:37 -0400

    Update App.test.js
    test added

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,12 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
